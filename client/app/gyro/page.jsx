88df8368ccede3facf5b023be1e847f162584271
anvu2002 <anvu6027@gmail.com>
2024-08-03 14:45:45 -0400

    working local gyro data

--- client/app/gyro/page.jsx
+++ client/app/gyro/page.jsx
@@ -1,38 +1,49 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useRef } from 'react';
 import GyroButton from '@/components/GyroButton';
 import Countdown from '@/components/Countdown';
+import { useRouter } from 'next/navigation';
 
 const Page = () => {
-
   const [started, setStarted] = useState(false);
-  const sessionData = []
+  const gyroDataRef = useRef([]);
+  const router = useRouter();
 
   const onGyroError = ({ error }) => {
-    alert("An error occurred starting the session!");
-    console.error(error)
-  }
+    alert('An error occurred starting the session!');
+    console.error(error);
+  };
 
   const onGyroData = ({ data }) => {
-    sessionData.push(data);
-  }
+    gyroDataRef.current.push(data);
+  };
 
   const onStart = () => {
-    setStarted(true)
-    window.s = sessionData
-  }
+    setStarted(true);
+  };
 
   const onEnd = () => {
-    alert("publish data")
-  }
+    localStorage.setItem('gyroData', JSON.stringify(gyroDataRef.current));
+   router.push('/profile');
+  };
 
   return (
     <div className="container mx-auto py-8 h-full-minus-navbar mt-[66px]">
       <h1>Gyro Testing</h1>
-      {started ? <div><Countdown duration={5} onEndCallback={onEnd}></Countdown></div> : <div><GyroButton onErrorCallback={onGyroError} onDataCallback={onGyroData} onClickCallback={onStart} /></div>}
-
+      {started ? (
+        <div>
+          <Countdown duration={5} onEndCallback={onEnd}></Countdown>
+        </div>
+      ) : (
+        <div>
+          <GyroButton
+            onErrorCallback={onGyroError}
+            onDataCallback={onGyroData}
+            onClickCallback={onStart}
+          />
+        </div>
+      )}
     </div>
   );
 };
