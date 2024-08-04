f2b9dccb522f63f6d1db76a52727927363eb62a7
anvu2002 <anvu6027@gmail.com>
2024-08-04 09:28:15 -0400

    deploying

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    distDir: 'build',
+  };
+  
+  export default nextConfig;
+  
